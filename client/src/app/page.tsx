9eb40d99f5fab7b16c1cf42237f0a69618fb6246
spikecodes <19519553+spikecodes@users.noreply.github.com>
2024-04-20 11:14:20 -0700

    🔀 Add Jay's code for Auth0

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,6 +4,11 @@ import Voice from "./Voice";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <a href="/api/auth/login">Login</a>
+        <br />
+        <a href="/api/auth/logout">Logout</a>
+      </div>
       <Voice />
     </main>
   );
