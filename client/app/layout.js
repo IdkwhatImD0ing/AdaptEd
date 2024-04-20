5edc14af3b4afd8ba4e0521f665a0bff1e4097c2
Jay Wu <jayqw@uci.edu>
2024-04-20 04:03:07 -0700

    Mongo and Auth0 working

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <UserProvider>
+        <body className={inter.className}>{children}</body>
+      </UserProvider>
     </html>
   );
 }
