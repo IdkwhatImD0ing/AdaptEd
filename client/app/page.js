148a1ef85aa8746c13ddbd982a8fd1d92281d197
Restyled.io <commits@restyled.io>
2024-04-20 11:04:59 +0000

    Restyled by clang-format

--- client/app/page.js
+++ client/app/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
+
 import Login from "./login/login.jsx";
 
 export default function Home() {
-  return (
-    <div>
-      <a href="/api/auth/login">Login</a>
+  return (<div><a href = "/api/auth/login">Login</a>
       <br />
-      <a href="/api/auth/logout">Logout</a>
+          <a href = "/api/auth/logout">Logout</a>
 
-    </div>
-  );
+    </div>);
 }
