a479528f1ac7cd8cf796e3d116d827413b03f831
Restyled.io <commits@restyled.io>
2024-04-20 11:05:01 +0000

    Restyled by prettier

--- client/app/page.js
+++ client/app/page.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 import Login from "./login/login.jsx";
 
 export default function Home() {
-  return (<div><a href = "/api/auth/login">Login</a>
+  return (
+    <div>
+      <a href="/api/auth/login">Login</a>
       <br />
-          <a href = "/api/auth/logout">Logout</a>
-
-    </div>);
+      <a href="/api/auth/logout">Logout</a>
+    </div>
+  );
 }
