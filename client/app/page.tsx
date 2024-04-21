f7019d7dafb71981b3d2b31e496118db15b06d01
Jasmine <jasmineqw45@gmail.com>
2024-04-21 01:15:55 -0700

    fleshed out hero of landing page and added the input page background

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,22 +2,30 @@ import Image from "next/image";
 import Voice from "./Voice";
 import NavBar from "./navBar/NavBar";
 import { WandSparkles, Sparkles } from "lucide-react";
+import NavBarLanding from "./NavBarLanding";
 export default function Home() {
   return (
-    <main className="hero">
-      <div className="tag">
-        <WandSparkles />
-        conversational learning
-      </div>
-      <h1 className="title">
-        Learn through conversations.
-        <br />
-        Not textbooks.
-      </h1>
-      <button className="primary">
-        {" "}
-        <Sparkles /> Make magic
-      </button>
-    </main>
+    <div>
+      <NavBarLanding />
+      <main className="hero">
+        <div className="tag">
+          <WandSparkles />
+          conversational learning
+        </div>
+        <h1 className="title">
+          Learn through conversations.
+          <br />
+          Not textbooks.
+        </h1>
+        <button className="primary">
+          {" "}
+          <Sparkles /> Make magic
+        </button>
+      </main>
+      <section>
+        <img src="./mockupLanding_adapted.svg" alt="" /> 
+      </section>
+     
+    </div>
   );
 }
