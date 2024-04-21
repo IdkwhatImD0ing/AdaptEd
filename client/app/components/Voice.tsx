50d9b5a4a3e5d8d950e02cea74ea57c2cd4f8e8b
Spike O'Carroll <19519553+spikecodes@users.noreply.github.com>
2024-04-20 17:39:07 -0700

    🐛 Fix Voice being mounted twice

--- client/app/components/Voice.tsx
+++ client/app/components/Voice.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import { RetellWebClient } from "retell-client-js-sdk";
 
-const AGENT_ID = "975ff855c8eaf42285e7c1e1a115c396";
+const AGENT_ID = "094ec635562bfaa326741f43e55db95d";
 
 const SERVER_ENDPOINT =
   "http://localhost:8000/voice/register-call-on-your-server";
@@ -20,22 +20,29 @@ type MessageTranscript = {
   content: string;
 };
 
-const App = () => {
+export default function Voice(props: {
+  onFuncCallResult: (result: any) => void;
+}) {
   const [isCalling, setIsCalling] = useState(false);
   const [userSpeaking, setUserSpeaking] = useState(false);
-  const [loading, setLoading] = useState(false);
 
   const retellClientRef = useRef<RetellWebClient>();
   const funcCallSocketRef = useRef<WebSocket>();
 
   useEffect(() => {
+    // If SDK already initialized
+    if (retellClientRef.current) {
+      return;
+    }
+
+    console.log("Mounted");
+
     // Initialize the SDK
     retellClientRef.current = new RetellWebClient();
 
     // Setup event listeners
     retellClientRef.current.on("conversationStarted", () => {
       console.log("conversationStarted");
-      setLoading(true);
     });
 
     retellClientRef.current.on("audio", (_audio: Uint8Array) => {
@@ -46,7 +53,6 @@ const App = () => {
     retellClientRef.current.on("conversationEnded", ({ code, reason }) => {
       console.log("Closed with code:", code, ", reason:", reason);
       setUserSpeaking(false);
-      setLoading(false);
       setIsCalling(false); // Update button to "Start" when conversation ends
     });
 
@@ -92,8 +98,14 @@ const App = () => {
     };
 
     funcCallSocketRef.current.onmessage = (event) => {
-      const funcCallResult = JSON.parse(event.data);
-      console.log("Function call result:", funcCallResult);
+      try {
+        const funcCallResult: FunctionCall = JSON.parse(event.data);
+
+        console.log("Function call result:", funcCallResult);
+        props.onFuncCallResult(funcCallResult);
+      } catch (error) {
+        console.error("Error parsing function call result:", error);
+      }
     };
 
     funcCallSocketRef.current.onclose = () => {
@@ -114,6 +126,7 @@ const App = () => {
             enableUpdate: true,
           })
           .catch(console.error);
+        console.log("Started call");
         setIsCalling(true); // Update button to "Stop" when conversation starts
       }
     }
@@ -149,22 +162,5 @@ const App = () => {
     }
   }
 
-  return (
-    <button onClick={toggleConversation} className="text-white ">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 256 256"
-        fill="currentColor"
-        width={50}
-        height={50}
-        // className="w-4 h-4"
-      >
-        <g transform="scale(10.66667,10.66667)">
-          <path d="M18.25 11a.75.75 0 0 1 .743.648l.007.102v.5a6.75 6.75 0 0 1-6.249 6.732l-.001 2.268a.75.75 0 0 1-1.493.102l-.007-.102v-2.268a6.75 6.75 0 0 1-6.246-6.496L5 12.25v-.5a.75.75 0 0 1 1.493-.102l.007.102v.5a5.25 5.25 0 0 0 5.034 5.246l.216.004h.5a5.25 5.25 0 0 0 5.246-5.034l.004-.216v-.5a.75.75 0 0 1 .75-.75M12 2a4 4 0 0 1 4 4v6a4 4 0 0 1-8 0V6a4 4 0 0 1 4-4m0 1.5A2.5 2.5 0 0 0 9.5 6v6a2.5 2.5 0 0 0 5 0V6A2.5 2.5 0 0 0 12 3.5" />
-        </g>
-      </svg>
-    </button>
-  );
-};
-
-export default App;
+  return <></>;
+}
