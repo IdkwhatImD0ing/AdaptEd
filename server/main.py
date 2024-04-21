636bdccf0f7eb40df84198a71438a3edffd6c544
Restyled.io <commits@restyled.io>
2024-04-21 13:41:22 +0000

    Restyled by reorder-python-imports

--- server/main.py
+++ server/main.py
@@ -6,7 +6,11 @@ import generate_route
 import voice
 from aggregate import generate
 from dotenv import load_dotenv
-from fastapi import FastAPI, Form, HTTPException, Request, WebSocket
+from fastapi import FastAPI
+from fastapi import Form
+from fastapi import HTTPException
+from fastapi import Request
+from fastapi import WebSocket
 from fastapi.middleware.cors import CORSMiddleware
 from manager import ConnectionManager
 from mongodb_manager import MongoDBManager
