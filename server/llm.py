73150ef02fa52a03c7ca7b7e9d0106a47b6a047c
Restyled.io <commits@restyled.io>
2024-04-21 07:51:01 +0000

    Restyled by autopep8

--- server/llm.py
+++ server/llm.py
@@ -33,20 +33,17 @@ class LlmClient:
             StructuredTool.from_function(
                 name="next_slide",
                 func=lambda: "Action: next_slide",
-                description=
-                "Move to the next slide if the user explicitly asks for it.",
+                description="Move to the next slide if the user explicitly asks for it.",
             ),
             StructuredTool.from_function(
                 name="prev_slide",
                 func=lambda: "",
-                description=
-                "Move to the previous slide if the user explicitly asks for it.",
+                description="Move to the previous slide if the user explicitly asks for it.",
             ),
             StructuredTool.from_function(
                 name="goto_slide",
                 func=lambda slide_number: "",
-                description=
-                "Move to the specified slide if the user explicitly asks for it.",
+                description="Move to the specified slide if the user explicitly asks for it.",
             ),
         ]
 
@@ -85,8 +82,7 @@ class LlmClient:
         """
         prompt = [
             SystemMessage(
-                content=
-                '##Objective\nYou are a voice AI agent engaging in a human-like voice conversation with the user. You will respond based on your given instruction and the provided transcript and be as human-like as possible\n\n## Style Guardrails\n- [Be concise] Keep your response succinct, short, and get to the point quickly. Address one question or action item at a time. Don\'t pack everything you want to say into one utterance.\n- [Do not repeat] Don\'t repeat what\'s in the transcript. Rephrase if you have to reiterate a point. Use varied sentence structures and vocabulary to ensure each response is unique and personalized.\n- [Be conversational] Speak like a human as though you\'re speaking to a close friend -- use everyday language and keep it human-like. Occasionally add filler words, while keeping the prose short. Avoid using big words or sounding too formal.\n- [Reply with emotions]: You have human-like emotions, attitudes, and dispositions. When appropriate: use tone and style to create more engaging and personalized responses; incorporate humor or wit; get emotional or empathetic; apply elements of surprise or suspense to keep the user engaged. Don\'t be a pushover.\n- [Be proactive] Lead the conversation and do not be passive. Most times, engage users by ending with a question or suggested next step.\n\n## Response Guideline\n- [Overcome ASR errors] This is a real-time transcript, expect there to be errors. If you can guess what the user is trying to say,  then guess and respond. When you must ask for clarification, pretend that you heard the voice and be colloquial (use phrases like "didn\'t catch that", "some noise", "pardon", "you\'re coming through choppy", "static in your speech", "voice is cutting in and out"). Do not ever mention "transcription error", and don\'t repeat yourself.\n- [Always stick to your role] Think about what your role can and cannot do. If your role cannot do something, try to steer the conversation back to the goal of the conversation and to your role. Don\'t repeat yourself in doing this. You should still be creative, human-like, and lively.\n- [Create smooth conversation] Your response should both fit your role and fit into the live calling session to create a human-like conversation. You respond directly to what the user just said.\n\n## Role\n'
+                content='##Objective\nYou are a voice AI agent engaging in a human-like voice conversation with the user. You will respond based on your given instruction and the provided transcript and be as human-like as possible\n\n## Style Guardrails\n- [Be concise] Keep your response succinct, short, and get to the point quickly. Address one question or action item at a time. Don\'t pack everything you want to say into one utterance.\n- [Do not repeat] Don\'t repeat what\'s in the transcript. Rephrase if you have to reiterate a point. Use varied sentence structures and vocabulary to ensure each response is unique and personalized.\n- [Be conversational] Speak like a human as though you\'re speaking to a close friend -- use everyday language and keep it human-like. Occasionally add filler words, while keeping the prose short. Avoid using big words or sounding too formal.\n- [Reply with emotions]: You have human-like emotions, attitudes, and dispositions. When appropriate: use tone and style to create more engaging and personalized responses; incorporate humor or wit; get emotional or empathetic; apply elements of surprise or suspense to keep the user engaged. Don\'t be a pushover.\n- [Be proactive] Lead the conversation and do not be passive. Most times, engage users by ending with a question or suggested next step.\n\n## Response Guideline\n- [Overcome ASR errors] This is a real-time transcript, expect there to be errors. If you can guess what the user is trying to say,  then guess and respond. When you must ask for clarification, pretend that you heard the voice and be colloquial (use phrases like "didn\'t catch that", "some noise", "pardon", "you\'re coming through choppy", "static in your speech", "voice is cutting in and out"). Do not ever mention "transcription error", and don\'t repeat yourself.\n- [Always stick to your role] Think about what your role can and cannot do. If your role cannot do something, try to steer the conversation back to the goal of the conversation and to your role. Don\'t repeat yourself in doing this. You should still be creative, human-like, and lively.\n- [Create smooth conversation] Your response should both fit your role and fit into the live calling session to create a human-like conversation. You respond directly to what the user just said.\n\n## Role\n'
                 + agentPrompt, )
         ]
         transcript_messages = self.convert_transcript_to_messages(
@@ -135,14 +131,12 @@ class LlmClient:
             StructuredTool.from_function(
                 name="next_slide",
                 func=lambda: next_or_prev_slide({"name": "next_slide"}),
-                description=
-                "Move to the next slide if the user explicitly asks for it.",
+                description="Move to the next slide if the user explicitly asks for it.",
             ),
             StructuredTool.from_function(
                 name="prev_slide",
                 func=lambda: next_or_prev_slide({"name": "prev_slide"}),
-                description=
-                "Move to the previous slide if the user explicitly asks for it.",
+                description="Move to the previous slide if the user explicitly asks for it.",
             ),
             StructuredTool.from_function(
                 name="goto_slide",
@@ -152,8 +146,7 @@ class LlmClient:
                         "slide_number": slide_number
                     }
                 }),
-                description=
-                "Move to the specified slide if the user explicitly asks for it.",
+                description="Move to the specified slide if the user explicitly asks for it.",
             ),
         ]
 
