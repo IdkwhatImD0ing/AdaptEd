c351f966063ab05df31c0f4c4604774c7be9c5d2
Jasmine <jasmineqw45@gmail.com>
2024-04-21 03:05:57 -0700

    added animations to landing

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,26 +1,68 @@
 "use client";
-
+import Fade from "react-reveal/Fade";
 import { WandSparkles, Sparkles } from "lucide-react";
 import NavBarLanding from "./NavBarLanding";
+import { TypeAnimation } from "react-type-animation";
 export default function Home() {
   return (
     <div>
-      <NavBarLanding />
+      <Fade top delay={1800}>
+        <NavBarLanding />
+      </Fade>
       <main className="hero">
-        <div className="tag">
-          <WandSparkles />
-          conversational learning
-        </div>
-        <h1 className="title">
-          Learn through conversations.
-          <br />
-        </h1>
-        <h1 className="text-4xl font-bold">Learn anything</h1>
-        <input type="text" placeholder="type a topic..." />
-        <button type="submit">Submit form</button>
+        <Fade bottom delay={200}>
+          <div className="tag">
+            <WandSparkles />
+            conversational learning
+          </div>
+        </Fade>
+        <Fade bottom delay={600}>
+          <h1 className="title">
+            Learn through conversations.
+            <br />
+          </h1>
+        </Fade>
+        <Fade bottom delay={1000}>
+          <h1>
+            Not
+            <TypeAnimation
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                "Textbooks.",
+                3000, // wait 1s before replacing "Mice" with "Hamsters"
+                "Lectures.",
+                3000,
+                "Articles.",
+                3000,
+              ]}
+              wrapper="span"
+              speed={30}
+              style={{
+                paddingLeft: "18px",
+                fontSize: "72px",
+                display: "inline-block",
+              }}
+              repeat={Infinity}
+            />
+          </h1>
+        </Fade>
+        <Fade bottom delay={1400}>
+          <button className="primary" style={{ marginTop: "32px" }}>
+            <Sparkles />
+            Make magic
+          </button>
+        </Fade>
       </main>
       <section>
-        <img src="./mockupLanding_adapted.svg" alt="" />
+        <img
+          src="./mockupLanding_adapted.svg"
+          style={{
+            marginTop: "110vh",
+            marginLeft: "10vw",
+            position: "absolute",
+            top: "0",
+          }}
+        />
       </section>
     </div>
   );
